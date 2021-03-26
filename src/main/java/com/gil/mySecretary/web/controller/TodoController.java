e6713727f58fcc5a8234a20326054748677f6239
ghpark <ghpark@openit.co.kr>
2021-03-26 17:59:10 +0900

    mariaDB setting

--- src/main/java/com/gil/mySecretary/web/controller/TodoController.java
+++ src/main/java/com/gil/mySecretary/web/controller/TodoController.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 import java.time.LocalDate;
 import java.util.List;
 
